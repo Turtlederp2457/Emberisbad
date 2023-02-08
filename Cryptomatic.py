813f7e2df19e4f874cc64c38f9bcfc2d9197be08
Dakota Pruett <Turtlederp2457@gmail.com>
2023-02-08 18:59:21 -0500

    Just work

--- Cryptomatic.py
+++ Cryptomatic.py
@@ -1,12 +1,49 @@
-plaintxtFile = open("/home/dakota/Documents/Ember_is_bad/Lincoln.txt", "r")
-charList = []
+#Author:Dakota Pruett 
+#Date:2/8/2023
+#For a class
 
-for line in plaintxtFile:
-    for char in line:
-        charList.append(char)
+def encrypt(plainList, keyPair, encryptList):
+    """
+    Only accepts Binary text
+    @plainList This is the binary text to be encrypted
+    @keyPair This is the binary key values in a list
+    @encryptList This is the list you wish for the output to be added to
+    """
+    keyIndex = 0
+    for num in plainList:
+        encryptList.append(XORnum(plainList[num], keyPair[keyIndex]))
+        if keyIndex==0: keyIndex=1
+        else: keyIndex=0
+    return 0
+
+def decrypt():
+    return 0
+
+def toBinary(inputChar):
+    return (bin(ord(inputChar))[2:])
+
+def toList(inputString, outputList):
+    for symbol in inputString:
+        outputList.append(symbol)
+    return "working"
+
+def XOR (a,b):
+    if a==0 and b==0: return 0
+    if a==1 and b==0: return 1
+    if a==0 and b==1: return 1
+    if a==1 and b==1: return 0
     
-for character in charList:
-    print(bin(ord(character))[2:])
-
+def XORnum (a,b):
+    outList = []
+    for i in a:
+        outList.append(XOR(a[i], b[i]))
+    return outList
+        
+#========================Start of logic=======================
+plaintxtFile = open("Lincoln.txt", "r")
 key = "MU"
+plainChars = []
+for line in plaintxtFile:
+    for achar in line:
+        plainChars.append(achar)
 
